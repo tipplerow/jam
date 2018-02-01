146a34b490fbe3c80cd0827976c7bfe5ec20b279
Scott Shaffer <jsshaff@berkeley.edu>
2018-01-31 17:08:00 -0800

    Facilitate the indexing of Ordinal objects

---
+++ src/jam/lang/OrdinalIndex.java
@@ -0,0 +1,45 @@
+
+package jam.lang;
+
+/**
+ * Encapsulates the management of ordinal indexes.
+ */
+public final class OrdinalIndex {
+    private long count = 0;
+
+    private OrdinalIndex() {}
+
+    /**
+     * Returns a new ordinal index initialized to zero.
+     *
+     * @return a new ordinal index initialized to zero.
+     */
+    public static OrdinalIndex create() {
+        return new OrdinalIndex();
+    }
+
+    /**
+     * Returns the index of the next object to be indexed.
+     *
+     * @return the index of the next object to be indexed.
+     */
+    public long next() {
+        return count++;
+    }
+
+    @Override public boolean equals(Object that) {
+        return (that instanceof OrdinalIndex) && equalsOrdinalIndex((OrdinalIndex) that);
+    }
+
+    private boolean equalsOrdinalIndex(OrdinalIndex that) {
+        return this.count == that.count;
+    }
+
+    @Override public int hashCode() {
+        return (int) count;
+    }
+
+    @Override public String toString() {
+        return String.format("OrdinalIndex(%d)", count);
+    }
+}
