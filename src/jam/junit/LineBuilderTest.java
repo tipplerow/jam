a4c0da65ef0d07f58854e4ca3eca42ded89c59ba
tipplerow <jsshaff@berkeley.edu>
2018-07-14 11:45:44 -0700

    Assist in building delimited report lines

---
+++ src/jam/junit/LineBuilderTest.java
@@ -0,0 +1,33 @@
+
+package jam.junit;
+
+import jam.report.LineBuilder;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class LineBuilderTest {
+    @Test public void testBuilder() {
+        LineBuilder builder = LineBuilder.csv();
+        assertEquals("", builder.toString());
+
+        builder.append(123);
+        assertEquals("123", builder.toString());
+
+        builder.append(345L);
+        assertEquals("123,345", builder.toString());
+
+        builder.append(1.23);
+        assertEquals("123,345,1.23", builder.toString());
+
+        builder.append(3.45E-8);
+        assertEquals("123,345,1.23,3.45E-8", builder.toString());
+
+        builder.append(3.45E-8, "%14.10f");
+        assertEquals("123,345,1.23,3.45E-8,  0.0000000345", builder.toString());
+    }
+
+    public static void main(String[] args) {
+        org.junit.runner.JUnitCore.main("jam.junit.LineBuilderTest");
+    }
+}
---
+++ src/jam/report/LineBuilder.java
@@ -0,0 +1,90 @@
+
+package jam.report;
+
+/**
+ * Builds delimited report lines field by field.
+ */
+public final class LineBuilder {
+    private final String delim;
+    private final StringBuilder builder;
+
+    // Number of fields already added to the line...
+    private int fieldCount = 0;
+
+    /**
+     * Creates a new line builder with a fixed delimiter.
+     *
+     * @param delim the field delimiter.
+     */
+    public LineBuilder(String delim) {
+        this.delim = delim;
+        this.builder = new StringBuilder();
+    }
+
+    /**
+     * Creates a new comma-delimted line builder.
+     *
+     * @return a new comma-delimted line builder.
+     */
+    public static LineBuilder csv() {
+        return new LineBuilder(",");
+    }
+
+    private void newField() {
+        //
+        // The delimiter is added only after the first field...
+        // 
+        if (fieldCount > 0)
+            builder.append(delim);
+
+        ++fieldCount;
+    }
+
+    /**
+     * Appends a {@code double} value to the line with default
+     * formatting.
+     *
+     * @param d the {@code double} value to append.
+     */
+    public void append(double d) {
+        newField();
+        builder.append(d);
+    }
+
+    /**
+     * Appends a {@code double} value to the line with specific
+     * formatting.
+     *
+     * @param d the {@code double} value to append.
+     *
+     * @param fmt the format string.
+     */
+    public void append(double d, String fmt) {
+        newField();
+        builder.append(String.format(fmt, d));
+    }
+
+    /**
+     * Appends an {@code int} value to the line.
+     *
+     * @param i the {@code int} value to append.
+     */
+    public void append(int i) {
+        newField();
+        builder.append(i);
+    }
+
+    /**
+     * Appends an {@code long} value to the line.
+     *
+     * @param lng the {@code long} value to append.
+     */
+    public void append(long lng) {
+        newField();
+        builder.append(lng);
+    }
+
+    @Override public String toString() {
+        return builder.toString();
+    }
+}
