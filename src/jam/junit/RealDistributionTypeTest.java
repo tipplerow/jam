38c272776815f228a22c693a1afdffdbb44de84b
Scott Shaffer <jsshaff@berkeley.edu>
2018-09-26 07:09:03 -0700

    Create distributions by parsing a descriptive string

--- src/jam/junit/RealDistributionTypeTest.java
+++ src/jam/junit/RealDistributionTypeTest.java
@@ -16,14 +16,14 @@ public class RealDistributionTypeTest extends NumericTestBase {
 
     @Test public void testDiracDelta() {
         DiracDeltaDistribution dist1 = new DiracDeltaDistribution(1.0);
-        DiracDeltaDistribution dist2 = (DiracDeltaDistribution) RealDistributionType.DIRAC_DELTA.create("1.0");
+        DiracDeltaDistribution dist2 = (DiracDeltaDistribution) RealDistributionType.parse("DIRAC_DELTA; 1.0");
 
         assertDouble(dist1.getImpulse(), dist2.getImpulse());
     }
 
     @Test public void testLogNormal() {
         LogNormalDistribution dist1 = new LogNormalDistribution(-1.0, 3.0);
-        LogNormalDistribution dist2 = (LogNormalDistribution) RealDistributionType.LOG_NORMAL.create("-1.0, 3.0");
+        LogNormalDistribution dist2 = (LogNormalDistribution) RealDistributionType.parse("LOG_NORMAL; -1.0, 3.0");
 
         assertDouble(dist1.getMeanLog(),  dist2.getMeanLog());
         assertDouble(dist1.getStDevLog(), dist2.getStDevLog());
@@ -31,7 +31,7 @@ public class RealDistributionTypeTest extends NumericTestBase {
 
     @Test public void testLogUniform() {
         LogUniformDistribution dist1 = new LogUniformDistribution(-1.0, 3.0);
-        LogUniformDistribution dist2 = (LogUniformDistribution) RealDistributionType.LOG_UNIFORM.create("-1.0, 3.0");
+        LogUniformDistribution dist2 = (LogUniformDistribution) RealDistributionType.parse("LOG_UNIFORM; -1.0, 3.0");
 
         assertDouble(dist1.getLowerLog(), dist2.getLowerLog());
         assertDouble(dist1.getUpperLog(), dist2.getUpperLog());
@@ -39,7 +39,7 @@ public class RealDistributionTypeTest extends NumericTestBase {
 
     @Test public void testNormal() {
         NormalDistribution dist1 = new NormalDistribution(-1.0, 3.0);
-        NormalDistribution dist2 = (NormalDistribution) RealDistributionType.NORMAL.create("-1.0, 3.0");
+        NormalDistribution dist2 = (NormalDistribution) RealDistributionType.parse("NORMAL; -1.0, 3.0");
 
         assertDouble(dist1.mean(),  dist2.mean());
         assertDouble(dist1.stdev(), dist2.stdev());
@@ -47,7 +47,7 @@ public class RealDistributionTypeTest extends NumericTestBase {
 
     @Test public void testUniform() {
         UniformRealDistribution dist1 = new UniformRealDistribution(-1.0, 3.0);
-        UniformRealDistribution dist2 = (UniformRealDistribution) RealDistributionType.UNIFORM.create("-1.0, 3.0");
+        UniformRealDistribution dist2 = (UniformRealDistribution) RealDistributionType.parse("UNIFORM; -1.0, 3.0");
 
         assertDouble(dist1.getLower(), dist2.getLower());
         assertDouble(dist1.getUpper(), dist2.getUpper());
