9eabed015a10c255cf4c5713018f640964eebd40
tipplerow <jsshaff@berkeley.edu>
2018-12-01 13:50:07 -0800

    Uniquely define the top-level directory for the project

---
+++ src/jam/app/JamHome.java
@@ -0,0 +1,19 @@
+
+package jam.app;
+
+import java.io.File;
+
+/**
+ * Locates the top-level directory of the {@code JAM} library.
+ */
+public final class JamHome {
+    /**
+     * The name of the top-level directory of the {@code JAM} library.
+     */
+    public static final String NAME = System.getenv("JAM_HOME");
+
+    /**
+     * The top-level directory of the {@code JAM} library.
+     */
+    public static final File DIR = new File(NAME);
+}
