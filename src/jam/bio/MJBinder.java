cb12ed01829f20d07307577320611e0dbb4d721d
tipplerow <jsshaff@berkeley.edu>
2018-12-07 06:10:51 -0800

    More specialized binders

---
+++ src/jam/bio/MJBinder.java
@@ -0,0 +1,18 @@
+
+package jam.bio;
+
+/**
+ * Represents peptide binders with <em>position-independent</em> local
+ * pairwise interactions defined by the Miyazawa-Jernigan potential.
+ */
+public interface MJBinder extends RIMBinder {
+    /**
+     * Returns the residue-interaction matrix that defines the
+     * pairwise potential.
+     *
+     * @return the pairwise free energy.
+     */
+    @Override public default RIM getRIM() {
+        return RIM.MiyazawaJernigan;
+    }
+}
