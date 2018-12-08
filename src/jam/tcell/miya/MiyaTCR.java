0c4cfec1f1cf67b832c95a3a1b293ffaded05156
tipplerow <jsshaff@berkeley.edu>
2018-12-07 19:10:10 -0800

    Miyazawa-Jernigan T cell repertoire simulation

---
+++ src/jam/tcell/miya/MiyaTCR.java
@@ -0,0 +1,72 @@
+
+package jam.tcell.miya;
+
+import jam.bio.MJBinder;
+import jam.bio.Peptide;
+import jam.lang.ObjectFactory;
+import jam.math.IntRange;
+import jam.tcell.GlobalTCR;
+import jam.tcell.TCellProperties;
+
+/**
+ * Represents a T cell receptor that binds peptides with a free energy
+ * defined by the Miyazawa-Jernigan pairwise potential.
+ */
+public final class MiyaTCR extends GlobalTCR implements MJBinder {
+    private final Peptide binder;
+
+    /**
+     * Creates a new Miyazawa-Jernigan T cell receptor with a fixed
+     * binding structure.
+     *
+     * @param binder the peptide binding structure.
+     */
+    public MiyaTCR(Peptide binder) {
+        this.binder = binder;
+    }
+
+    /**
+     * A factory that creates Miyazawa-Jernigan T cell receptors with
+     * randomly generated binding structures having the common global
+     * length.
+     */
+    public static ObjectFactory<MiyaTCR> GLOBAL_FACTORY = new GlobalFactory();
+
+    private static final class GlobalFactory implements ObjectFactory<MiyaTCR> {
+        @Override public MiyaTCR newInstance() {
+            return MiyaTCR.newInstance();
+        }
+    }
+
+    /**
+     * Creates a new Miyazawa-Jernigan T cell receptor with a randomly
+     * generated binding structure with the common global length.
+     *
+     * @return a new Miyazawa-Jernigan T cell receptor with a randomly
+     * generated binding structure with the common global length.
+     */
+    public static MiyaTCR newInstance() {
+        return newInstance(TCellProperties.getReceptorLength());
+    }
+
+    /**
+     * Creates a new Miyazawa-Jernigan T cell receptor with a randomly
+     * generated binding structure of a given length.
+     *
+     * @param length the length of the binding structure.
+     *
+     * @return a new Miyazawa-Jernigan T cell receptor with a randomly
+     * generated binding structure having the specified length.
+     */
+    public static MiyaTCR newInstance(int length) {
+        return new MiyaTCR(Peptide.newNative(length));
+    }
+
+    @Override public Peptide getBinderPeptide() {
+        return binder;
+    }
+
+    @Override public IntRange getTargetRegion() {
+        return TCellProperties.getTargetRegion();
+    }
+}
---
+++ src/jam/tcell/miya/MiyaTuner.java
@@ -0,0 +1,27 @@
+
+package jam.tcell.miya;
+
+import jam.bio.Peptide;
+import jam.lang.ObjectFactory;
+import jam.tcell.TCR;
+import jam.tcell.TCellProperties;
+import jam.tcell.ThresholdTuner;
+
+public final class MiyaTuner extends ThresholdTuner {
+    private MiyaTuner(String[] propertyFiles) {
+        super(propertyFiles);
+    }
+
+    @Override protected ObjectFactory<? extends Peptide> getPeptideFactory() {
+        return Peptide.nativeFactory(TCellProperties.getReceptorLength());
+    }
+
+    @Override protected ObjectFactory<? extends TCR> getTCRFactory() {
+        return MiyaTCR.GLOBAL_FACTORY;
+    }
+
+    public static void main(String[] args) {
+        MiyaTuner tuner = new MiyaTuner(args);
+        tuner.run();
+    }
+}
