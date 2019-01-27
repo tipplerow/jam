69aae04e219bd42fdbc1904731a7f4d01a9ad923
tipplerow <jsshaff@berkeley.edu>
2019-01-27 14:19:13 -0800

    Use a SQLite database as a persistent cache

--- src/jam/pepmhc/AffinityCache.java
+++ src/jam/pepmhc/AffinityCache.java
@@ -1,58 +1,246 @@
 
 package jam.pepmhc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import jam.app.JamLogger;
+import jam.app.JamProperties;
+import jam.io.FileUtil;
+import jam.lang.JamException;
 import jam.peptide.Peptide;
+import jam.util.MapUtil;
 
 public final class AffinityCache {
+    private final String allele;
     private final PepMHCPredictor predictor;
-    private final Map<String, Map<Peptide, BindingRecord>> alleleMap;
+    private final PredictionMethod method;
+    private final Connection connection;
+    private final Map<Peptide, BindingRecord> recordMap;
 
-    public AffinityCache(PepMHCPredictor predictor) {
-        this.predictor = predictor;
-        this.alleleMap = new HashMap<String, Map<Peptide, BindingRecord>>();
+    private static final Map<PredictionMethod, Map<String, AffinityCache>> instances =
+        new EnumMap<PredictionMethod, Map<String, AffinityCache>>(PredictionMethod.class);
+
+    private static final String TABLE_NAME = "affinity";
+
+    private static final String KEY_NAME = "peptide";
+    private static final String KEY_TYPE = "string";
+
+    private static final String AFFINITY_NAME = "affinity";
+    private static final String AFFINITY_TYPE = "double";
+
+    private static final String PERCENTILE_NAME = "percentile";
+    private static final String PERCENTILE_TYPE = "double";
+
+    private static final String TABLE_SCHEMA =
+        String.format("%s %s PRIMARY KEY, %s %s, %s %s",
+                      KEY_NAME, KEY_TYPE,
+                      AFFINITY_NAME, AFFINITY_TYPE,
+                      PERCENTILE_NAME, PERCENTILE_TYPE);
+
+    private static final String CREATE_TABLE_COMMAND =
+        String.format("CREATE TABLE IF NOT EXISTS %s (%s)", TABLE_NAME, TABLE_SCHEMA);
+
+    private static final String SELECT_ALL_QUERY =
+        String.format("SELECT %s, %s, %s FROM %s", KEY_NAME, AFFINITY_NAME, PERCENTILE_NAME, TABLE_NAME);
+
+    private static final String INSERT_RECORD_TEMPLATE =
+        "INSERT INTO %s VALUES('%s', %s, %s)";
+
+    private AffinityCache(PredictionMethod method, String allele) {
+        this.method = method;
+        this.allele = allele;
+        this.predictor = PepMHCPredictor.instance(method);
+        this.recordMap = new HashMap<Peptide, BindingRecord>();
+        this.connection = openConnection();
+
+        loadTable();
     }
 
-    public Map<Peptide, BindingRecord> lookup(String allele, Collection<Peptide> peptides) {
-        Collection<Peptide> computeList = new ArrayList<Peptide>();
+    public static final String CACHE_DIRECTORY_PROPERTY = "jam.pepmhc.cacheDirectory";
 
-        Map<Peptide, BindingRecord> innerMap  = getInnerMap(allele);
-        Map<Peptide, BindingRecord> resultMap = new HashMap<Peptide, BindingRecord>(peptides.size());
-
-        for (Peptide peptide : peptides) {
-            BindingRecord record = innerMap.get(peptide);
-
-            if (record != null)
-                resultMap.put(peptide, record);
-            else
-                computeList.add(peptide);
+    private Connection openConnection() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(formatURL());
         }
+        catch (Exception ex) {
+            throw JamException.runtime(ex);
+        }
+    }
 
-        if (!computeList.isEmpty()) {
-            List<BindingRecord> newRecords = predictor.predict(allele, computeList);
+    private String formatURL() {
+        return "jdbc:sqlite:" + resolveDbFile();
+    }
 
-            for (BindingRecord newRecord : newRecords) {
-                innerMap.put(newRecord.getPeptide(), newRecord);
-                resultMap.put(newRecord.getPeptide(), newRecord);
+    private String resolveDbFile() {
+        return FileUtil.join(resolveDbDir(), formatDbFile());
+    }
+
+    private static String resolveDbDir() {
+        return JamProperties.getRequired(CACHE_DIRECTORY_PROPERTY);
+    }
+
+    private String formatDbFile() {
+        return String.format("%s_%s.db", method, formatAllele());
+    }
+
+    private String formatAllele() {
+        return allele.replace("*", "-").replace(":", "-");
+    }
+
+    private void loadTable() {
+        try (Statement statement = connection.createStatement()) {
+            initTable(statement);
+            loadTable(statement);
+        }
+        catch (SQLException ex) {
+            throw JamException.runtime(ex);
+        }
+    }
+
+    private void initTable(Statement statement) throws SQLException {
+        //
+        // Create the table if necessary...
+        //
+        statement.executeUpdate(CREATE_TABLE_COMMAND);
+    }
+
+    private void loadTable(Statement statement) throws SQLException {
+        try (ResultSet resultSet = statement.executeQuery(SELECT_ALL_QUERY)) {
+            while (resultSet.next()) {
+                loadRow(resultSet);
             }
         }
 
-        return resultMap;
+        JamLogger.info("Loaded [%d] records from the affinity database...", recordMap.size());
     }
 
-    private Map<Peptide, BindingRecord> getInnerMap(String allele) {
-        Map<Peptide, BindingRecord> innerMap = alleleMap.get(allele);
+    private void loadRow(ResultSet resultSet) throws SQLException {
+        Peptide peptide    = Peptide.parse(resultSet.getString(KEY_NAME));
+        double  affinity   = resultSet.getDouble(AFFINITY_NAME);
+        double  percentile = resultSet.getDouble(PERCENTILE_NAME);
 
-        if (innerMap == null) {
-            innerMap = new HashMap<Peptide, BindingRecord>();
-            alleleMap.put(allele, innerMap);
+        cacheRecord(new BindingRecord(peptide, affinity, percentile));
+    }
+
+    private void cacheRecord(BindingRecord record) {
+        recordMap.put(record.getPeptide(), record);
+    }
+
+    public static BindingRecord get(PredictionMethod method, String allele, Peptide peptide) {
+        return get(method, allele, List.of(peptide)).get(0);
+    }
+
+    public static List<BindingRecord> get(PredictionMethod method, String allele, Collection<Peptide> peptides) {
+        return instance(method, allele).get(peptides);
+    }
+
+    private static AffinityCache instance(PredictionMethod method, String allele) {
+        Map<String, AffinityCache> cacheMap = instances.get(method);
+
+        if (cacheMap == null) {
+            JamLogger.info("Creating cache map for method [%s]...", method);
+            cacheMap = new HashMap<String, AffinityCache>();
+            instances.put(method, cacheMap);
         }
 
-        return innerMap;
+        AffinityCache cache = cacheMap.get(allele);
+
+        if (cache == null) {
+            JamLogger.info("Creating cache for allele [%s]...", allele);
+            cache = new AffinityCache(method, allele);
+            cacheMap.put(allele, cache);
+        }
+
+        return cache;
+    }
+
+    private List<BindingRecord> get(Collection<Peptide> peptides) {
+        //
+        // Identify peptides from the input collection that are not
+        // present in the cache ("missing" peptides)...
+        //
+        List<Peptide> missing = flagMissing(peptides);
+
+        JamLogger.info("[%d] peptides cached, [%d] require computation...",
+                       peptides.size() - missing.size(), missing.size());
+
+        if (!missing.isEmpty()) {
+            // Compute affinity for each missing peptide...
+            List<BindingRecord> records = predictor.predict(allele, missing);
+
+            // Add the new records to the cache and the database
+            // table...
+            updateCache(records);
+            updateTable(records);
+        }
+
+        // Pull the records from the cache in the order requested...
+        return MapUtil.get(recordMap, peptides);
+    }
+
+    private List<Peptide> flagMissing(Collection<Peptide> peptides) {
+        List<Peptide> targets = new ArrayList<Peptide>();
+
+        for (Peptide peptide : peptides)
+            if (!recordMap.containsKey(peptide))
+                targets.add(peptide);
+
+        return targets;
+    }
+
+    private void updateCache(List<BindingRecord> records) {
+        JamLogger.info("Adding [%s] records to the in-memory map...", records.size());
+
+        for (BindingRecord record : records)
+            cacheRecord(record);
+    }
+
+    private void updateTable(List<BindingRecord> records) {
+        JamLogger.info("Adding [%s] records to the database table...", records.size());
+
+        try (Statement statement = connection.createStatement()) {
+            for (BindingRecord record : records)
+                insertRecord(statement, record);
+        }
+        catch (SQLException ex) {
+            //
+            // No need to halt the program...
+            //
+            JamLogger.warn("Failed to update database table!");
+        }
+    }
+
+    private void insertRecord(Statement statement, BindingRecord record) throws SQLException {
+        String insertCommand =
+            String.format(INSERT_RECORD_TEMPLATE,
+                          TABLE_NAME,
+                          formatPeptide(record.getPeptide()),
+                          formatDouble(record.getAffinity()),
+                          formatDouble(record.getPercentile()));
+
+        statement.executeUpdate(insertCommand);
+    }
+
+    private static String formatPeptide(Peptide peptide) {
+        return peptide.formatString();
+    }
+
+    private static String formatDouble(double value) {
+        if (Double.isNaN(value))
+            return "null";
+        else
+            return Double.toString(value);
     }
 }
