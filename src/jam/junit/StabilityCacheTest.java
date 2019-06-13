4dc812996f2026cccf06684359ec466d841dcc56
tipplerow <jsshaff@berkeley.edu>
2019-06-13 14:32:21 -0700

    Adding a cache for stability records

---
+++ src/jam/junit/StabilityCacheTest.java
@@ -0,0 +1,35 @@
+
+package jam.junit;
+
+import jam.hla.Allele;
+import jam.peptide.Peptide;
+
+import jam.stab.NetStab;
+import jam.stab.StabilityCache;
+import jam.stab.StabilityRecord;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class StabilityCacheTest {
+    static {
+        System.setProperty(StabilityCache.CACHE_DIRECTORY_PROPERTY, "data/cache");
+    }
+
+    private final Allele allele = Allele.instance("HLA-A*02:01");
+
+    @Test public void testNetMHC() {
+        if (!NetStab.isInstalled())
+            return;
+
+        StabilityRecord record = StabilityCache.get(allele, Peptide.parse("AEFGPWQTV"));
+        System.out.println(record);
+
+        StabilityCache.get(allele, Peptide.parse("AEFGPWQTV"));
+        StabilityCache.get(allele, Peptide.parse("AEFGPWQTV"));
+    }
+
+    public static void main(String[] args) {
+        org.junit.runner.JUnitCore.main("jam.junit.StabilityCacheTest");
+    }
+}
---
+++ src/jam/stab/StabilityBatchProcess.java
@@ -0,0 +1,32 @@
+
+package jam.stab;
+
+import java.util.Collection;
+import java.util.List;
+
+import jam.hla.Allele;
+import jam.peptide.Peptide;
+import jam.process.BatchProcess;
+
+final class StabilityBatchProcess extends BatchProcess<Peptide, StabilityRecord> {
+    private final Allele allele;
+
+    private final static int BATCH_SIZE = 100000;
+
+    private StabilityBatchProcess(Allele allele, Collection<Peptide> peptides) {
+        super(peptides, BATCH_SIZE);
+        this.allele = allele;
+    }
+
+    static List<StabilityRecord> run(Allele allele, Collection<Peptide> peptides) {
+        StabilityBatchProcess process =
+            new StabilityBatchProcess(allele, peptides);
+
+        return process.runSequential();
+    }
+
+    @Override public List<StabilityRecord> runBatch(Collection<Peptide> inputSlice) {
+        return NetStab.run(allele, inputSlice);
+    }
+}
+
