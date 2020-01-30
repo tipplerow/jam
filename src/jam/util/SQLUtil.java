7295aec01cdf7522ed4d78e8eb70648a5b1bbc0a
tipplerow <jsshaff@berkeley.edu>
2020-01-30 13:52:41 -0800

    Adding SQL utilities

---
+++ src/jam/util/SQLUtil.java
@@ -0,0 +1,163 @@
+
+package jam.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import jam.app.JamLogger;
+import jam.lang.JamException;
+
+/**
+ * Provides utility methods to facilitate SQL operations.
+ */
+public final class SQLUtil {
+    /**
+     * Opens a new database connection.
+     *
+     * @param url the URL for the database.
+     *
+     * @return an open database connection for the specified URL.
+     *
+     * @throws RuntimeException unless the connection can be opened.
+     */
+    public static Connection open(String url) {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(url);
+        }
+        catch (Exception ex) {
+            throw JamException.runtime(ex);
+        }
+    }
+
+    /**
+     * Closes a database connection.
+     *
+     * @param connection the connection to close.
+     */
+    public static void close(Connection connection) {
+        try {
+            connection.close();
+        }
+        catch (SQLException ex) {
+            JamLogger.info("Failed to close database connection: [%s].", ex.getMessage());
+        }
+    }
+
+    /**
+     * Counts the number of rows that contain a given integer value.
+     *
+     * @param connection an open database connection.
+     *
+     * @param tableName the name of the table to query.
+     *
+     * @param columnName the name of the integer column.
+     *
+     * @param value the integer value to count.
+     */
+    public static int count(Connection connection, String tableName, String columnName, int value) {
+        String queryString =
+            String.format("SELECT COUNT(*) FROM %s WHERE %s = %d", tableName, columnName, value);
+
+        return count(connection, queryString);
+    }
+
+    /**
+     * Counts the number of rows that contain a given string value.
+     *
+     * @param connection an open database connection.
+     *
+     * @param tableName the name of the table to query.
+     *
+     * @param columnName the name of the string column.
+     *
+     * @param value the string value to count.
+     */
+    public static int count(Connection connection, String tableName, String columnName, String value) {
+        String queryString =
+            String.format("SELECT COUNT(*) FROM %s WHERE %s = '%s'", tableName, columnName, value);
+            
+        return count(connection, queryString);
+    }
+
+    private static int count(Connection connection, String queryString) {
+        int count = 0;
+
+        try {
+            ResultSet resultSet =
+                executeQuery(connection, queryString);
+
+            resultSet.next();
+            count = resultSet.getInt(1);
+            resultSet.close();
+        }
+        catch (SQLException ex) {
+            throw JamException.runtime(ex);
+        }
+
+        return count;
+    }
+
+    /**
+     * Creates a database table if it does not already exist.
+     *
+     * @param connection an open database connection.
+     *
+     * @param tableName the name of the new table.
+     *
+     * @param tableSchema the schema for the new table.
+     *
+     * @throws RuntimeException if any errors occur.
+     */
+    public static void createTable(Connection connection, String tableName, String tableSchema) {
+        executeUpdate(connection, String.format("CREATE TABLE IF NOT EXISTS %s (%s)", tableName, tableSchema));
+    }
+
+    /**
+     * Executes a database query.
+     *
+     * @param connection an open database connection.
+     *
+     * @param query the query string.
+     *
+     * @return the result set returned by the database.
+     *
+     * @throws RuntimeException if any errors occur.
+     */
+    public static ResultSet executeQuery(Connection connection, String query) {
+        try {
+            Statement statement = connection.createStatement();
+            return statement.executeQuery(query);
+        }
+        catch (SQLException ex) {
+            throw JamException.runtime(ex);
+        }
+    }
+
+    /**
+     * Executes a database update.
+     *
+     * @param connection an open database connection.
+     *
+     * @param update the update string.
+     *
+     * @return the result set returned by the database.
+     *
+     * @throws RuntimeException if any errors occur.
+     */
+    public static void executeUpdate(Connection connection, String update) {
+        try {
+            Statement statement =
+                connection.createStatement();
+
+            statement.executeUpdate(update);
+            statement.close();
+        }
+        catch (SQLException ex) {
+            throw JamException.runtime(ex);
+        }
+    }
+}
