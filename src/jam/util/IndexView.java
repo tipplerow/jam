355e25e87d392fcb654ac52ab27188a71aba930a
tipplerow <jsshaff@berkeley.edu>
2020-02-18 17:23:13 -0800

    Override additional Map methods

--- src/jam/util/IndexView.java
+++ src/jam/util/IndexView.java
@@ -41,7 +41,39 @@ public abstract class IndexView<K, V> extends AbstractMap<K, V> {
         this.map = Collections.unmodifiableMap(map);
     }
 
+    @Override public boolean containsKey(Object key) {
+        return map.containsKey(key);
+    }
+
+    @Override public boolean containsValue(Object value) {
+        return map.containsValue(value);
+    }
+
     @Override public Set<Map.Entry<K, V>> entrySet() {
         return map.entrySet();
+    }
+
+    @Override public V get(Object key) {
+        return map.get(key);
+    }
+
+    @Override public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override public Set<K> keySet() {
+        return map.keySet();
+    }
+
+    @Override public int size() {
+        return map.size();
+    }
+
+    @Override public Collection<V> values() {
+        return map.values();
+    }
+
+    @Override public String toString() {
+        return map.toString();
     }
 }
