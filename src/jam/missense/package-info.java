4321bf7141f57003d5c9101b65885705025486ec
tipplerow <jsshaff@berkeley.edu>
2020-03-08 08:56:49 -0700

    Adding package info

---
+++ src/jam/missense/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Processes missense mutations arising from single nucleotide variants.
+ */
+package jam.missense;
---
+++ src/jam/sql/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides compute-on-demand, in-memory caching backed by a SQL database.
+ */
+package jam.sql;
