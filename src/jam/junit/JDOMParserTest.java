3f13f2588f55c954c37f35e6a52a0e89ff5a3d5a
tipplerow <jsshaff@berkeley.edu>
2020-04-06 16:19:36 -0700

    Adding a JDOM parser base class

---
+++ src/jam/junit/JDOMParserTest.java
@@ -0,0 +1,39 @@
+
+package jam.junit;
+
+import java.util.List;
+
+import org.jdom2.Element;
+
+import jam.xml.JDOMDocument;
+import jam.xml.JDOMParser;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class JDOMParserTest {
+    private static final String DESC_SAMPLE_FILE = "data/test/mesh_desc_sample.xml";
+
+    @Test public void testParser() {
+        JDOMParser parser = new JDOMParser();
+        JDOMDocument document = JDOMDocument.parse(DESC_SAMPLE_FILE);
+
+        parser.assertTagName(document.getRootElement(), "DescriptorRecordSet");
+
+        List<Element> descriptorList = document.getRootElementChildren();
+        assertEquals(3, descriptorList.size());
+
+        for (Element descriptorElement : descriptorList)
+            parser.assertTagName(descriptorElement, "DescriptorRecord");
+
+        assertEquals("1", parser.getRequiredAttribute(descriptorList.get(0), "DescriptorClass"));
+        assertEquals("bar", parser.getOptionalAttribute(descriptorList.get(0), "foo", "bar"));
+
+        assertEquals(1, parser.getIntAttribute(descriptorList.get(0), "DescriptorClass"));
+        assertEquals(2, parser.getIntAttribute(descriptorList.get(0), "foo", 2));
+    }
+
+    public static void main(String[] args) {
+        org.junit.runner.JUnitCore.main("jam.junit.JDOMParserTest");
+    }
+}
---
+++ src/jam/xml/JDOMParser.java
@@ -0,0 +1,200 @@
+
+package jam.xml;
+
+import org.jdom2.Element;
+
+import jam.lang.JamBoolean;
+import jam.lang.JamException;
+
+/**
+ * Provides a base class for customized JDOM parsers.
+ */
+public class JDOMParser {
+    /**
+     * Asserts that a given element has a specific tag.
+     *
+     * @param element the element to validate.
+     *
+     * @param tagName the expected tag name.
+     *
+     * @throws RuntimeException unless the specified element has the
+     * specified tag name.
+     */
+    public void assertTagName(Element element, String tagName) {
+        if (!element.getName().equals(tagName))
+            throw JamException.runtime("Expected element [%s], but found [%s].",
+                                       tagName, element.getName());
+    }
+
+    /**
+     * Returns an optional named attribute from the attribute list
+     * of an element.
+     *
+     * @param element the element to examine.
+     *
+     * @param attrName the name of the desired attribute.
+     *
+     * @param defaultVal the default value to be used for missing
+     * attributes.
+     *
+     * @return the string value of the desired attribute, if present,
+     * or the default value otherwise.
+     */
+    public String getOptionalAttribute(Element element, String attrName, String defaultVal) {
+        String attrValue = element.getAttributeValue(attrName);
+
+        if (attrValue != null)
+            return attrValue;
+        else
+            return defaultVal;
+    }
+
+    /**
+     * Returns a required named attribute from the attribute list
+     * of an element.
+     *
+     * @param element the element to examine.
+     *
+     * @param attrName the name of the desired attribute.
+     *
+     * @return the string value of the desired attribute.
+     *
+     * @throws RuntimeException unless the desired attribute is
+     * present.
+     */
+    public String getRequiredAttribute(Element element, String attrName) {
+        String attrValue = element.getAttributeValue(attrName);
+
+        if (attrValue != null)
+            return attrValue;
+        else
+            throw JamException.runtime("Missing required attribute: [%s:%s].",
+                                       element.getName(), attrName);
+    }
+
+    /**
+     * Returns a required boolean attribute from the attribute list of
+     * an element.
+     *
+     * @param element the element to examine.
+     *
+     * @param attrName the name of the desired attribute.
+     *
+     * @return the boolean value of the desired attribute.
+     *
+     * @throws RuntimeException unless the desired attribute is
+     * present and is a valid boolean value.
+     */
+    public boolean getBooleanAttribute(Element element, String attrName) {
+        return JamBoolean.valueOf(getRequiredAttribute(element, attrName));
+    }
+
+    /**
+     * Returns an optional boolean attribute from the attribute list
+     * of an element.
+     *
+     * @param element the element to examine.
+     *
+     * @param attrName the name of the desired attribute.
+     *
+     * @param defaultVal the value to be used for missing attributes.
+     *
+     * @return the boolean value of the desired attribute, or the
+     * default value if the attribute is not present.
+     *
+     * @throws RuntimeException if the desired attribute is present
+     * but is not a valid boolean value.
+     */
+    public boolean getBooleanAttribute(Element element, String attrName, boolean defaultVal) {
+        String attrValue = element.getAttributeValue(attrName);
+
+        if (attrValue != null)
+            return JamBoolean.valueOf(attrValue);
+        else
+            return defaultVal;
+    }
+
+    /**
+     * Returns a required double attribute from the attribute list of
+     * an element.
+     *
+     * @param element the element to examine.
+     *
+     * @param attrName the name of the desired attribute.
+     *
+     * @return the double value of the desired attribute.
+     *
+     * @throws RuntimeException unless the desired attribute is
+     * present and is a valid double value.
+     */
+    public double getDoubleAttribute(Element element, String attrName) {
+        return Double.parseDouble(getRequiredAttribute(element, attrName));
+    }
+
+    /**
+     * Returns an optional double attribute from the attribute list
+     * of an element.
+     *
+     * @param element the element to examine.
+     *
+     * @param attrName the name of the desired attribute.
+     *
+     * @param defaultVal the value to be used for missing attributes.
+     *
+     * @return the double value of the desired attribute, or the
+     * default value if the attribute is not present.
+     *
+     * @throws RuntimeException if the desired attribute is present
+     * but is not a valid double value.
+     */
+    public double getDoubleAttribute(Element element, String attrName, double defaultVal) {
+        String attrValue = element.getAttributeValue(attrName);
+
+        if (attrValue != null)
+            return Double.parseDouble(attrValue);
+        else
+            return defaultVal;
+    }
+
+    /**
+     * Returns a required integer attribute from the attribute list of
+     * an element.
+     *
+     * @param element the element to examine.
+     *
+     * @param attrName the name of the desired attribute.
+     *
+     * @return the integer value of the desired attribute.
+     *
+     * @throws RuntimeException unless the desired attribute is
+     * present and is a valid integer value.
+     */
+    public int getIntAttribute(Element element, String attrName) {
+        return Integer.parseInt(getRequiredAttribute(element, attrName));
+    }
+
+    /**
+     * Returns an optional integer attribute from the attribute list
+     * of an element.
+     *
+     * @param element the element to examine.
+     *
+     * @param attrName the name of the desired attribute.
+     *
+     * @param defaultVal the value to be used for missing attributes.
+     *
+     * @return the integer value of the desired attribute, or the
+     * default value if the attribute is not present.
+     *
+     * @throws RuntimeException if the desired attribute is present
+     * but is not a valid integer value.
+     */
+    public int getIntAttribute(Element element, String attrName, int defaultVal) {
+        String attrValue = element.getAttributeValue(attrName);
+
+        if (attrValue != null)
+            return Integer.parseInt(attrValue);
+        else
+            return defaultVal;
+    }
+}
