02eb22d4e5abbf2097d78353abe3d477a3c934f9
tipplerow <jsshaff@berkeley.edu>
2020-04-10 15:55:30 -0700

    Adding the 100 most common English words

--- src/jam/util/TextUtil.java
+++ src/jam/util/TextUtil.java
@@ -3,11 +3,37 @@ package jam.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Provides utility classes for operating on free text.
  */
 public final class TextUtil {
+    /**
+     * The 100 most common words in English.
+     */
+    public static final Set<String> TOP_100 =
+        Set.of("a", "about", "all", "also", "and",
+               "as", "at", "be", "because", "but",
+               "by", "can", "come", "could", "day",
+               "do", "even", "find", "first", "for",
+               "from", "get", "give", "go", "have",
+               "he", "her", "here", "him", "his",
+               "how", "I", "if", "in", "into",
+               "it", "its", "just", "know", "like",
+               "look", "make", "man", "many", "me",
+               "more", "my", "new", "no", "not",
+               "now", "of", "on", "one", "only",
+               "or", "other", "our", "out", "people",
+               "say", "see", "she", "so", "some",
+               "take", "tell", "than", "that", "the",
+               "their", "them", "then", "there", "these",
+               "they", "thing", "think", "this", "those",
+               "time", "to", "two", "up", "use",
+               "very", "want", "way", "we", "well",
+               "what", "when", "which", "who", "will",
+               "with", "would", "year", "you", "your");
+
     /**
      * Splits text into words.
      *
